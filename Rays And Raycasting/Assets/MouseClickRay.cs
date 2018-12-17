e286e6bbb77f36859584474be69e4ad192a8c0f1
Richard <r.banks@burnley.ac.uk>
2018-12-17 14:33:06 +0000

    Mouse Ray Casting

---
+++ Rays And Raycasting/Assets/MouseClickRay.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseClickRay : MonoBehaviour
+{
+
+	public GameObject impactParticles;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			CastRay();
+		}
+	}
+
+	void CastRay()
+	{
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+		
+		
+		if (Physics.Raycast(ray, out hit))
+		{
+			if (hit.collider.CompareTag("Enemy"))
+			{
+				//hit.collider.GetComponent<MeshRenderer>().material.color = Color.red;
+				hit.collider.GetComponent<EnemyScript>().TakeDamage(5);
+				Instantiate(impactParticles, hit.point, Quaternion.identity);
+
+			}
+		}
+	}
+}
---
+++ Rays And Raycasting/Assets/UIRotateToward.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIRotateToward : MonoBehaviour
+{
+	private RectTransform rect;
+
+	// Use this for initialization
+	void Start ()
+	{
+		rect = GetComponent<RectTransform>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		Vector3 relPos = Camera.main.transform.position - transform.position;
+		
+		Quaternion rot = Quaternion.LookRotation(relPos, Vector3.up);
+
+		rect.rotation = rot;
+	}
+}
