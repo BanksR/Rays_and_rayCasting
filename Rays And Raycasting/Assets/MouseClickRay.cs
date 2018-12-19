def7f4f0d043948a4c2c22a82b34c74ea5c6f586
Richard <r.banks@burnley.ac.uk>
2018-12-19 09:59:16 +0000

    Updated commenting...

--- Rays And Raycasting/Assets/MouseClickRay.cs
+++ Rays And Raycasting/Assets/MouseClickRay.cs
@@ -4,35 +4,42 @@ using UnityEngine;
 
 public class MouseClickRay : MonoBehaviour
 {
-
+	// This script generates a raycast based on mouseposition / click
+	// This is a fundamental piece of code to underestand
+	
+	// Impact particles
 	public GameObject impactParticles;
 	
-	// Use this for initialization
-	void Start () 
-	{
-		
-	}
+	
 	
 	// Update is called once per frame
 	void Update ()
 	{
+		// If our player clicks the left mouse button - run our CastRay() function...
 		if (Input.GetMouseButtonDown(0))
 		{
 			CastRay();
 		}
 	}
 
+	
 	void CastRay()
 	{
+		// Here we declare a new variable of type Ray called ray - and initialise it using a function of the Camera class
+		// that generates a ray based on a screen position - in our case - the mouse position when the function was called
 		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		
+		// A new Raycast hit object - here we find all the useful hit info / data
 		RaycastHit hit;
 		
 		
+		// Testing our raycast hit - what did it hit?
 		if (Physics.Raycast(ray, out hit))
 		{
 			if (hit.collider.CompareTag("Enemy"))
 			{
-				//hit.collider.GetComponent<MeshRenderer>().material.color = Color.red;
+				
+				// If we hit an enemy, we know we can grab it's EnemyScript class and do stuff to it
 				hit.collider.GetComponent<EnemyScript>().TakeDamage(5);
 				Instantiate(impactParticles, hit.point, Quaternion.identity);
 
